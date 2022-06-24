3650ca6f0667c6e909f76d54581d20f2dd64fb18
Frank Sinapi <frank.sinapi@opentrons.com>
2022-06-24 10:11:22 -0400

    Add namespace to buffer_type.hpp

--- include/ot_utils/core/buffer_type.hpp
+++ include/ot_utils/core/buffer_type.hpp
@@ -3,7 +3,13 @@
 #include <array>
 #include <cstdint>
 
+namespace ot_utils {
+namespace utils {
+
 template <typename Buffer>
 concept GenericByteBuffer = requires(Buffer buff) {
     {std::is_same_v<Buffer, std::array<uint8_t, sizeof(Buffer)>>};
 };
+
+}  // namespace utils
+}  // namespace ot_utils
